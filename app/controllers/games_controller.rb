09638e2a77399e8253c3a9bda5df9363209eaa53
Emanuel Flores <flores.emanuel.108@gmail.com>
2025-04-04 00:22:39 -0400

    game of words with rail v2

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,32 @@
+require 'open-uri'
+require 'json'
+require 'date'
+require 'time'
+
 class GamesController < ApplicationController
   def new
+    @grid = random_chars
+    session[:grid] = @grid
+  end
+
+  def score
+    @word = params[:word].upcase
+    url = "https://dictionary.lewagon.com/#{@word}"
+    json_file = URI.parse(url).read
+    json_doc = JSON.parse(json_file)
+    grid = session[:grid]
+
+    @phrase = ''
+    if !json_doc['found']
+      @phrase = "Sorry bug #{@word} doesn't seem to be a valid English word."
+    elsif json_doc['found'] && valid_chars?(@word, grid)
+      @phrase = "Congratulations! #{@word} is a valida English word!"
+    else
+      @phrase = "Sorry but #{@word} can't be built out of: #{grid.join(', ')}"
+    end
+  end
+
+  def random_chars
     letter = ('A'..'Z').to_a
     @grid = ''
     i = 0
@@ -8,10 +35,15 @@ class GamesController < ApplicationController
       @grid += letter.sample
       i += 1
     end
+
     @grid = @grid.chars
   end
 
-  def score
-    @word = params[:word]
+  def valid_chars?(word, grid)
+    grid.each do |letter|
+      word = word.sub(letter, '') if word.include?(letter)
+    end
+
+    true if word.length.zero?
   end
 end
